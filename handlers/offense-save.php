d56abf70dd5f028104a658c4479b4b65728d9ca3
jeypips <johnpaulbalanon6@gmail.com>
2017-12-12 15:31:38 +0800

    ..

--- handlers/offense-save.php
+++ handlers/offense-save.php
@@ -9,14 +9,15 @@ $con = new pdo_db("offenses");
 
 
 $_POST['offense']['student_no'] = $_POST['offense']['student_no']['student_id'];
-if (isset($_POST['inc_uniform']))    
+if (isset($_POST['offense']['inc_uniform'],$_POST['offense']['late_tardy'],$_POST['offense']['absent'],$_POST['offense']['no_id'],$_POST['offense']['cutting_classes'],$_POST['offense']['check_others']))    
 {    
-	$_POST['inc_uniform'] = ($_POST['inc_uniform'])?1:0; 
-	/* $_POST['late_tardy'] = ($_POST['late_tardy'])?1:0;	
-	$_POST['absent'] = ($_POST['absent'])?1:0;		
-	$_POST['no_id'] = ($_POST['no_id'])?1:0;	
-	$_POST['cutting_classes'] = ($_POST['cutting_classes'])?1:0;
-	$_POST['cutting_classes'] = ($_POST['check_others'])?1:0; */
+	$_POST['offense']['inc_uniform'] = ($_POST['offense']['inc_uniform'])?1:0; 
+	$_POST['offense']['late_tardy'] = ($_POST['offense']['late_tardy'])?1:0;	
+	$_POST['offense']['absent'] = ($_POST['offense']['absent'])?1:0;		
+	$_POST['offense']['no_id'] = ($_POST['offense']['no_id'])?1:0;	
+	$_POST['offense']['cutting_classes'] = ($_POST['offense']['cutting_classes'])?1:0;
+	$_POST['offense']['check_others'] = ($_POST['offense']['check_others'])?1:0;
+	$_POST['offense']['recom_others_cb'] = ($_POST['offense']['recom_others_cb'])?1:0;
 }
 
 $_POST['offense']['offs_date'] =  date("Y-m-d",strtotime($_POST['offense']['offs_date']));
