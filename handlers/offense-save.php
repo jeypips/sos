624e9e2a0357b936f7f463db83763029ba38e1db
Theroshi <theroshaballesteros@gmail.com>
2017-12-13 13:48:12 +0800

    .

--- handlers/offense-save.php
+++ handlers/offense-save.php
@@ -9,14 +9,19 @@ $con = new pdo_db("offenses");
 
 
 $_POST['offense']['student_no'] = $_POST['offense']['student_no']['student_id'];
-if (isset($_POST['offense']['inc_uniform'],$_POST['offense']['late_tardy'],$_POST['offense']['absent'],$_POST['offense']['no_id'],$_POST['offense']['cutting_classes'],$_POST['offense']['recom_others_cb'],$_POST['offense']['recom_others_cb'],$_POST['offense']['recommendation']))    
+if (isset($_POST['offense']['inc_uniform'],$_POST['offense']['late_tardy'],$_POST['offense']['absent'],$_POST['offense']['no_id'],$_POST['offense']['cutting_classes'],$_POST['offense']['recom_others_cb'],$_POST['offense']['recom_others_cb'],$_POST['offense']['admitted_excuse'],$_POST['offense']['admitted_notexcuse'],$_POST['offense']['academic_loses'],$_POST['offense']['completion_required'],$_POST['offense']['dropped'],$_POST['offense']['parent_notification']))    
 {    
 	$_POST['offense']['inc_uniform'] = ($_POST['offense']['inc_uniform'])?1:0; 
 	$_POST['offense']['late_tardy'] = ($_POST['offense']['late_tardy'])?1:0;	
 	$_POST['offense']['absent'] = ($_POST['offense']['absent'])?1:0;		
 	$_POST['offense']['no_id'] = ($_POST['offense']['no_id'])?1:0;	
 	$_POST['offense']['cutting_classes'] = ($_POST['offense']['cutting_classes'])?1:0;
-	$_POST['offense']['recommendation'] = ($_POST['offense']['recommendation'])?1:0;
+	$_POST['offense']['admitted_excuse'] = ($_POST['offense']['admitted_excuse'])?1:0;
+	$_POST['offense']['admitted_notexcuse'] = ($_POST['offense']['admitted_notexcuse'])?1:0;
+	$_POST['offense']['academic_loses'] = ($_POST['offense']['academic_loses'])?1:0;
+	$_POST['offense']['completion_required'] = ($_POST['offense']['completion_required'])?1:0;
+	$_POST['offense']['dropped'] = ($_POST['offense']['dropped'])?1:0;
+	$_POST['offense']['parent_notification'] = ($_POST['offense']['parent_notification'])?1:0;
 	$_POST['offense']['check_others'] = ($_POST['offense']['check_others'])?1:0;
 	$_POST['offense']['recom_others_cb'] = ($_POST['offense']['recom_others_cb'])?1:0;
 }
