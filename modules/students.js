d61a5cb67d2bccd96b3a47bfade08fa1427369f7
jeypips <johnpaulbalanon6@gmail.com>
2018-01-23 08:10:49 +0800

    ..

--- modules/students.js
+++ modules/students.js
@@ -193,7 +193,10 @@ angular.module('students-module',['bootstrap-modal','bootstrap-growl','snapshot-
 				$timeout(function() {
 					$('#student').DataTable({
 						// "ordering": false,
-						"processing": true
+						 // "processing": true,
+						 "processing": true,
+						 "language": {
+						  processing: '<i class="fa fa-spinner fa-spin fa-3x fa-fw"></i><span class="sr-only">Loading...</span> '}						
 						
 					});	
 				},200);
